e191ed4bc90af642b2f2fa5b2cda138e760a5e83
yena <50291995+nyj001012@users.noreply.github.com>
2023-04-02 18:22:27 +0900

    update(philosophers): deleted unnecessary assignment

--- philo/util/print_util.c
+++ philo/util/print_util.c
@@ -6,7 +6,7 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 00:59:02 by yena              #+#    #+#             */
-/*   Updated: 2023/04/02 16:21:03 by yena             ###   ########.fr       */
+/*   Updated: 2023/04/02 17:57:53 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ void	print_eating(t_philosophers *philosophers)
 	if (philosophers->philo_info->is_end)
 		return ;
 	pthread_mutex_lock(&philosophers->philo_info->during_routine);
-	philosophers->number_of_times_eaten++;
 	time = get_time();
+	philosophers->number_of_times_eaten++;
 	philosophers->last_time_eaten = time;
 	time -= philosophers->philo_info->birthday;
-	printf("%lld ", time);
+	printf("%lld\t", time);
 	printf("%d\tis eating\n", philosophers->number);
 	pthread_mutex_unlock(&philosophers->philo_info->during_routine);
 }
@@ -35,9 +35,8 @@ void	print_sleeping(t_philosophers *philosophers)
 	if (philosophers->philo_info->is_end)
 		return ;
 	pthread_mutex_lock(&philosophers->philo_info->during_routine);
-	time = get_time();
-	time -= philosophers->philo_info->birthday;
-	printf("%lld ", time);
+	time = get_time() - philosophers->philo_info->birthday;
+	printf("%lld\t", time);
 	printf("%d\tis sleeping\n", philosophers->number);
 	pthread_mutex_unlock(&philosophers->philo_info->during_routine);
 }
@@ -49,9 +48,8 @@ void	print_thinking(t_philosophers *philosophers)
 	if (philosophers->philo_info->is_end)
 		return ;
 	pthread_mutex_lock(&philosophers->philo_info->during_routine);
-	time = get_time();
-	time -= philosophers->philo_info->birthday;
-	printf("%lld ", time);
+	time = get_time() - philosophers->philo_info->birthday;
+	printf("%lld\t", time);
 	printf("%d\tis thinking\n", philosophers->number);
 	pthread_mutex_unlock(&philosophers->philo_info->during_routine);
 }
@@ -62,21 +60,18 @@ void	print_death(t_philosophers *philosophers)
 
 	if (philosophers->philo_info->is_end)
 		return ;
-	time = get_time();
-	time -= philosophers->philo_info->birthday;
-	printf("%lld ", time);
-	printf("%d\tdead\n", philosophers->number);
+	time = get_time() - philosophers->philo_info->birthday;
+	printf("%lld\t", time);
+	printf("%d\tdied\n", philosophers->number);
 }
 
 void	print_fork(t_philosophers *philosophers)
 {
 	long long	time;
 
-	if (philosophers->philo_info->is_end)
-		return ;
 	pthread_mutex_lock(&philosophers->philo_info->during_routine);
 	time = get_time() -  philosophers->philo_info->birthday;
-	printf("%lld ", time);
+	printf("%lld\t", time);
 	printf("%d\thas taken a fork\n", philosophers->number);
 	pthread_mutex_unlock(&philosophers->philo_info->during_routine);
 }
