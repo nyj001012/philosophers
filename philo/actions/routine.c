03f4e19b8161f9edf71a47357f61505bcb60f01a
yena <50291995+nyj001012@users.noreply.github.com>
2023-04-02 18:23:02 +0900

    update(philosophers): tried optimization...

--- philo/actions/routine.c
+++ philo/actions/routine.c
@@ -6,7 +6,7 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 12:15:48 by yena              #+#    #+#             */
-/*   Updated: 2023/04/02 16:15:40 by yena             ###   ########.fr       */
+/*   Updated: 2023/04/02 18:19:13 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,16 @@
 
 void	take_fork(t_philosophers *philosophers)
 {
+	print_fork(philosophers);
+	print_fork(philosophers);
 	pthread_mutex_lock(philosophers->left_fork);
 	pthread_mutex_lock(philosophers->right_fork);
-	print_fork(philosophers);
-	print_fork(philosophers);
 }
 
 void	eat(t_philosophers *philosophers)
 {
+	print_eating(philosophers);
 	pthread_mutex_lock(&philosophers->critical_section);
-	if (!philosophers->philo_info->is_end)
-		print_eating(philosophers);
 	usleep(philosophers->philo_info->time_to_eat * 1000);
 	pthread_mutex_unlock(&philosophers->critical_section);
 	pthread_mutex_unlock(philosophers->left_fork);
@@ -40,4 +39,5 @@ void	do_sleep(t_philosophers *philosophers)
 void	think(t_philosophers *philosophers)
 {
 	print_thinking(philosophers);
+	usleep(100);
 }
